de502251a1aa1a7095e8f9646ecb2d3a51670d24
M2 <italiyankin@gmail.com>
2023-12-13 12:40:56 +0000

    Api advancements

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\ModuloController;
+use App\Http\Controllers\Auth\LoginRegisterController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -20,4 +21,29 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::resource('modulos', ModuloController::class);
+});
+
+
+// Public routes of authtication
+Route::controller(LoginRegisterController::class)->group(function() {
+    Route::post('/register', 'register');
+    Route::post('/login', 'login');
+});
+
+// Public routes of product
+Route::controller(ModuloController::class)->group(function() {
+    // Route::get('/products', 'index');
+    // Route::get('/products/{id}', 'show');
+    // Route::get('/products/search/{name}', 'search');
+});
+
+// Protected routes of product and logout
+Route::middleware('auth:sanctum')->group( function () {
+    Route::post('/logout', [LoginRegisterController::class, 'logout']);
+
+    Route::controller(ModuloController::class)->group(function() {
+        // Route::post('/products', 'store');
+        // Route::post('/products/{id}', 'update');
+        // Route::delete('/products/{id}', 'destroy');
+    });
 });
