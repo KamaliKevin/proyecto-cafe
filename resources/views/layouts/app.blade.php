52f32f4e5fea5587fae084476c4f9589291a24e4
M2 <italiyankin@gmail.com>
2023-12-17 22:17:34 +0000

    add bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
@@ -83,23 +83,23 @@
     </div>
 
     <!-- Bootstrap JS -->
-    <script src="{{ asset("js/bootstrap.min.js") }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
     <!-- Especialidades -->
-    <script src="{{ asset("js/specialties.js") }}"></script>
+    <script src="{{ asset('js/specialties.js') }}"></script>
     <!-- Departamentos -->
-    <script src="{{ asset("js/departments.js") }}"></script>
+    <script src="{{ asset('js/departments.js') }}"></script>
     <!-- Profesores -->
-    <script src="{{ asset("js/teachers.js") }}"></script>
+    <script src="{{ asset('js/teachers.js') }}"></script>
     <!-- Cursos -->
-    <script src="{{ asset("js/courses.js") }}"></script>
+    <script src="{{ asset('js/courses.js') }}"></script>
     <!-- Módulos -->
-    <script src="{{ asset("js/subjects.js") }}"></script>
+    <script src="{{ asset('js/subjects.js') }}"></script>
     <!-- Control de horario -->
-    <script src="{{ asset("js/main.js") }}"></script>
+    <script src="{{ asset('js/main.js') }}"></script>
     <!-- Control de departamento -->
-    <script src="{{ asset("js/departmentControl.js") }}"></script>
+    <script src="{{ asset('js/departmentControl.js') }}"></script>
     <!-- Control de estudios -->
-    <script src="{{ asset("js/studyControl.js") }}"></script>
+    <script src="{{ asset('js/studyControl.js') }}"></script>
 </body>
 
 </html>
