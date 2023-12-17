1159de7002a7ff147350a53ebc2a13038a83b566
M2 <italiyankin@gmail.com>
2023-12-17 22:40:57 +0000

    Finishing touches

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,7 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        \URL::forceScheme('https');
-        Paginator::useBootstrap();
+        if (config('app.env') === 'production') {
+            \URL::forceScheme('https');
+            Paginator::useBootstrap();
+        }
     }
 }
