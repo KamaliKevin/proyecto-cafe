9ddef178929d0c9747ff2142b5bbf54d43cdb8d4
M2 <italiyankin@gmail.com>
2023-12-17 12:24:25 +0000

    Department api

---
+++ app/Http/Controllers/Api/V1/DepartamentoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Departamento;
+use Illuminate\Http\Request;
+
+class DepartamentoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $departamentos = Departamento::with('user.modulos.curso')->get();
+
+        if (is_null($departamentos->first())) {
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'No product found!',
+            ], 200);
+        }
+
+        $response = [
+            'status' => 'success',
+            'message' => 'modulos are retrieved successfully.',
+            'data' => $departamentos,
+        ];
+
+        return response()->json($response, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Departamento $departamento)
+    {
+        return response()->json(Departamento::with('user.modulos.curso')->find($departamento->id), 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Departamento $departamento)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Departamento $departamento)
+    {
+        //
+    }
+}
